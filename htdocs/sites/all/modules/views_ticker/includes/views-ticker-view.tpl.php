d8e88bf735c4fd9036d3d61e88e2c7210c15c15d
dyouster <david.youster@teamaol.com>
2015-04-28 13:24:54 +0000

    Added header logo

--- htdocs/sites/all/modules/views_ticker/includes/views-ticker-view.tpl.php
+++ htdocs/sites/all/modules/views_ticker/includes/views-ticker-view.tpl.php
@@ -57,6 +57,78 @@ foreach ($rows as $row)
 	}
 }
 
+foreach ($rows as $row)
+{
+	if ($scroller_type == 'horizontal')
+	{
+		print "<li class='views-liScroll-item views-liScroll-item-$viewname'>";
+		print "<span class='views-liScroll-tick-field'>$row</span></li>";
+	}
+	elseif ($scroller_type == 'vertical')
+	{
+		print "<li class='views-vTicker-item views-vTicker-item-$viewname'>";
+		print "<span class='views-vTicker-tick-field'>$row</span></li>";
+	}
+	elseif($scroller_type=='fade')
+	{
+		print "<li class='views-fade-item views-fade-item-$viewname'>";
+		print "<span class='views-fade-tick-field'>$row</span></li>";
+	}
+	else #bbc
+	{
+		print "<li class='views-bbc-item views-bbc-item-$viewname'>";
+		print "<span class='views-bbc-tick-field'>$row</span></li>";
+	}
+}
+
+foreach ($rows as $row)
+{
+	if ($scroller_type == 'horizontal')
+	{
+		print "<li class='views-liScroll-item views-liScroll-item-$viewname'>";
+		print "<span class='views-liScroll-tick-field'>$row</span></li>";
+	}
+	elseif ($scroller_type == 'vertical')
+	{
+		print "<li class='views-vTicker-item views-vTicker-item-$viewname'>";
+		print "<span class='views-vTicker-tick-field'>$row</span></li>";
+	}
+	elseif($scroller_type=='fade')
+	{
+		print "<li class='views-fade-item views-fade-item-$viewname'>";
+		print "<span class='views-fade-tick-field'>$row</span></li>";
+	}
+	else #bbc
+	{
+		print "<li class='views-bbc-item views-bbc-item-$viewname'>";
+		print "<span class='views-bbc-tick-field'>$row</span></li>";
+	}
+}
+
+foreach ($rows as $row)
+{
+	if ($scroller_type == 'horizontal')
+	{
+		print "<li class='views-liScroll-item views-liScroll-item-$viewname'>";
+		print "<span class='views-liScroll-tick-field'>$row</span></li>";
+	}
+	elseif ($scroller_type == 'vertical')
+	{
+		print "<li class='views-vTicker-item views-vTicker-item-$viewname'>";
+		print "<span class='views-vTicker-tick-field'>$row</span></li>";
+	}
+	elseif($scroller_type=='fade')
+	{
+		print "<li class='views-fade-item views-fade-item-$viewname'>";
+		print "<span class='views-fade-tick-field'>$row</span></li>";
+	}
+	else #bbc
+	{
+		print "<li class='views-bbc-item views-bbc-item-$viewname'>";
+		print "<span class='views-bbc-tick-field'>$row</span></li>";
+	}
+}
+
 
 print "</ul></div></div></div>";
 
